fab96fbb67fc2295cff9804516c11041e37e43ce
Eben <pakaanezer@gmail.com>
2023-05-25 13:06:49 +0200

    Fastapi Commit 3

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Salut tout le monde...."}
+    return {"message": "Salut tout le monde, Mon API a été déployé avec succès..."}
